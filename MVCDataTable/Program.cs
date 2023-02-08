26929ff5c7f9bd93fecfbd9323a995ddaa5b164a
Asgar9 <asgaryas@code.edu.az>
2023-02-08 09:52:40 -0800

    Promlemed

---
+++ MVCDataTable/Program.cs
@@ -0,0 +1,31 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddDistributedMemoryCache();
+
+builder.Services.AddSession(options =>
+{
+    options.Cookie.Name = "CodeAcademy_Session_Name";
+    options.IdleTimeout = TimeSpan.FromSeconds(60);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+app.UseSession();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=ecommerce}/{action=index}/{id?}");
+
+app.Run();
---
+++ MVCDataTable/wwwroot/js/site.js
@@ -0,0 +1,79 @@
+﻿jQuery(document).ready(function ($) {
+
+    $('.add_to_cart_button').on('click', function () {
+        var id = $(this).data('elma');
+
+        $.ajax({
+            url: "/cart/add/" + id,
+            method: "POST",
+            success: (data) => { GetCart(); },
+            error: (err) => { console.log(err) }
+        });
+    })
+
+
+    function GetCart() {
+
+        $.getJSON('/cart/getall', function (data) {
+            let cart = [];
+            var total = 0;
+            var count = 0; 
+           
+            $.each(data, (key, value) => {
+
+                total += value.total;
+                count += value.count;
+
+                var template = ` 
+                    <div class="cart-product clearfix">
+                        <div class="cart-product-image">
+                            <a class="cart-product-img" href="#">
+                                <img width="300" height="300" src="images/products/product_60x60.jpg" alt=""/>
+                            </a>
+                        </div>
+                        <div class="cart-product-details">
+                            <div class="cart-product-title">
+                                <a href="#">${value.productName}</a>
+                            </div>
+                            <div class="cart-product-quantity-price">
+                                ${value.count} x <span class="amount">&pound;${value.total}</span>
+                            </div>
+                        </div>
+                        <a href="#" class="remove" title="Remove this item">&times;</a>
+                    </div>`;
+                cart.push(template);
+            })
+
+
+            let p1 = `
+                <div class="minicart">
+                    <div class="minicart-header">
+                        ${count} items in the shopping cart
+                    </div>
+                <div class="minicart-body">`;
+
+            let p2 = `
+                </div>
+                    <div class="minicart-footer">
+                        <div class="minicart-total">
+                            Cart Subtotal <span class="amount">&pound;${total}</span>
+                        </div>
+                        <div class="minicart-actions clearfix">
+                            <a class="viewcart-button button" href="#">
+                                <span class="text">View Cart</span>
+                            </a>
+                            <a class="checkout-button button" href="#">
+                                <span class="text">Checkout</span>
+                            </a>
+                        </div>
+                    </div>
+                </div>`
+
+            $('#cart-span').html(count)
+            $('.minicart-side-content').html(`${p1} ${cart.join(' ')} ${p2}`);
+
+        })
+    }
+
+    GetCart();
+})
